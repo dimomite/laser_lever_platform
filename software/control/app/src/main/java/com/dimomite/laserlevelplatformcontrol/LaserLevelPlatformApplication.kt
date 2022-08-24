067bf9f330bd423bf41b5586b3004783fd5f4dff
dimomite <dkuzkz@gmail.com>
2022-08-24 02:16:53 +0200

    Initial app commit
    Add control application initial setup.
    Add Hilt and RxJava to dependencies.

---
+++ software/control/app/src/main/java/com/dimomite/laserlevelplatformcontrol/LaserLevelPlatformApplication.kt
@@ -0,0 +1,8 @@
+package com.dimomite.laserlevelplatformcontrol
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LaserLevelPlatformApplication : Application() {
+}
